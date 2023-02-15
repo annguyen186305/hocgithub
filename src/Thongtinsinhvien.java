0f4b8033282ca7667dc7bf00458ce989e44fc8b1
annguyen186305 <annguyen186305@gmail.com>
2023-02-15 09:55:34 +0700

    update thong tin sinh vien

---
+++ src/Thongtinsinhvien.java
@@ -0,0 +1,18 @@
+public class Thongtinsinhvien {
+    public static void main(String[] args) {
+        String name;
+        Integer age;
+        Integer id;
+        name = "Nguyen Van An";
+        age = 20;
+        id = 21250613;
+        System.out.print("Day la sinh vien:");
+        System.out.println(name);
+        System.out.print("Tuoi:");
+        System.out.println(age);
+        System.out.print("Ma sinh vien:");
+        System.out.println(id);
+    }
+}
+
+
