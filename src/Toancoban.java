61126f2c394c350aecf90a97d8ac09cff16f7057
annguyen186305 <annguyen186305@gmail.com>
2023-02-15 18:29:21 +0700

    update toancoban

--- src/Toancoban.java
+++ src/Toancoban.java
@@ -1,7 +1,7 @@
 public class Toancoban {
     public static void main(String[] args) {
         int a = 1;
-        System.out.println(a++);
+        System.out.println(a---1);
         System.out.println(++a);
         System.out.println(a+++1);
         System.out.println(++a+1);
