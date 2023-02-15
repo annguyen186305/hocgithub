6f9d9a69fbd8c0ab12143c7eced4a362eee16e58
annguyen186305 <annguyen186305@gmail.com>
2023-02-15 10:34:30 +0700

    update toan hoc

---
+++ src/Toancoban.java
@@ -0,0 +1,11 @@
+public class Toancoban {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a+++1);
+        System.out.println(++a+1);
+
+    }
+}
+
