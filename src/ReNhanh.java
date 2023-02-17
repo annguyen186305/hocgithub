35f580be7b6645572219f819152a109b43284411
annguyen186305 <annguyen186305@gmail.com>
2023-02-17 22:48:46 +0700

    update cau truc re nhanh

---
+++ src/ReNhanh.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReNhanh {
+    public static void main(String[] args) {
+        String name = "An";
+        if (name == "An")
+            System.out.print("Dep zai nhat xom");
+        else if (name == "Dat")
+            System.out.print("Xau zai nhat xom");
+        else
+            System.out.print("khong co gi ca");
+    }
+}
---
+++ src/banchatcuaNext.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class banchatcuaNext {
+    public static void main(String[] args) {
+        int a, b, c, d, e;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập a: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        e = sc.nextInt();
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+    }
+}
+
